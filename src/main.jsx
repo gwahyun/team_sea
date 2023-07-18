44848fc2bb4d5ba54cc0f5476279f7f957e53104
gwahyun <60453182+gwahyun@users.noreply.github.com>
2023-07-18 19:25:14 +0900

    feat: vite config 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import {HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/team_sea">
+    <HashRouter basename="/team_sea">
     <div className='w-full h-screen flex justify-center items-center'>
     <App />
     </div>
+    </HashRouter>
 
-    </BrowserRouter>
- 
   </React.StrictMode>,
 )
