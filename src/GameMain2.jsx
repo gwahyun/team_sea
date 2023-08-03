a648060beaf4c9bd78163ac9e65e8bdef6b869a1
gwahyun <60453182+gwahyun@users.noreply.github.com>
2023-08-03 13:10:19 +0900

    Update GameMain2.jsx

--- src/GameMain2.jsx
+++ src/GameMain2.jsx
@@ -1,15 +1,16 @@
-import { useEffect } from 'react';
-import { useNavigate} from 'react-router-dom';
-import mainArt from './assets/2.gif';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import mainArt from "./assets/2.gif";
 
+export default function GameMain2() {
+  const navigate = useNavigate();
 
-export default function GameMain2 (){
-    const navigate = useNavigate();
-
-    useEffect(()=>{
-        setTimeout(()=> navigate('/start'), 2000);
-    },[])
-    return(<div className='flex justify-center items-center'>
-        <img src={mainArt}/>
-        </div>)
+  useEffect(() => {
+    setTimeout(() => navigate("/start"), 2000);
+  }, []);
+  return (
+    <div className="flex justify-center items-center">
+      <img className="w-[80%]" src={mainArt} />
+    </div>
+  );
 }
